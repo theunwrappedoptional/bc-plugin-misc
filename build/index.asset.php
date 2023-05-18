3d2a5ad88fc82bfec9dc19f4b63e9ea6dc5965d7
Marco Carraro <carra.rules@gmail.com>
2023-05-18 17:05:12 +0200

    test

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-i18n'), 'version' => '2aae6aaa5d0e00c29ccc');
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { __ } from '@wordpress/i18n';
+import './editor.scss';
+
+const x = 0;
+
+alert( __( 'Loaded!' ) );
