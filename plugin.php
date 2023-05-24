4b0b80707c68209dfbd79126feef5e60348b1a16
Marco Carraro <carra.rules@gmail.com>
2023-05-24 18:22:38 +0200

    Set metadata

--- plugin.php
+++ plugin.php
@@ -1,23 +1,23 @@
 <?php
 /**
- * Plugin Name:       Plugin Boilerplate
- * Description:       Plugin Boilerplate.
+ * Plugin Name:       Misc Plugin
+ * Description:       Misc Plugin.
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Version:           0.1.0
  * Author:            MC
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       plugin-boilerplate
+ * Text Domain:       plugin-misc
  *
  */
 
- function bc_plugin_boilerplate_enqueue_assets() {
+ function bc_plugin_misc_enqueue_assets() {
 
     $asset_file = include(plugin_dir_path( __FILE__ ).'build/index.asset.php');
 
-    wp_enqueue_script( 'bc-plugin-boilerplate-script', plugins_url('build/index.js', __FILE__), $asset_file['dependencies'], $asset_file['version']);
-    wp_enqueue_style( 'bc-plugin-boilerplate-style', plugins_url('build/index.css', __FILE__));
+    wp_enqueue_script( 'bc_plugin_misc-script', plugins_url('build/index.js', __FILE__), $asset_file['dependencies'], $asset_file['version']);
+    wp_enqueue_style( 'bc_plugin_misc-style', plugins_url('build/index.css', __FILE__));
  }
 
- add_action( 'enqueue_block_editor_assets', 'bc_plugin_boilerplate_enqueue_assets');
+ add_action( 'enqueue_block_editor_assets', 'bc_plugin_misc_enqueue_assets');
